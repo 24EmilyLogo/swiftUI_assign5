7d42388dabb878e31113954f7e329923b59bcd57
24EmilyLogo <142520367+24EmilyLogo@users.noreply.github.com>
2024-04-18 10:20:52 -0700

    Add files via upload

---
+++ swiftUI_assign6/swiftUI_assign6/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  swiftUI_assign6
+//
+//  Created by StudentAM on 4/17/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ swiftUI_assign6/swiftUI_assign6/swiftUI_assign6App.swift
@@ -0,0 +1,17 @@
+//
+//  swiftUI_assign6App.swift
+//  swiftUI_assign6
+//
+//  Created by StudentAM on 4/17/24.
+//
+
+import SwiftUI
+
+@main
+struct swiftUI_assign6App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
